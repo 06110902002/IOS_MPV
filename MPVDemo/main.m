ef8d500f3abf58da00d452ca7c3bfb83bf05ad60
刘玉兵 <505691285@qq.com>
2018-09-28 15:50:15 +0800

    Initial Commit

---
+++ MPVDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MPVDemo
+//
+//  Created by liuxiaobing on 2018/9/28.
+//  Copyright © 2018 dream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MPVDemoTests/MPVDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  MPVDemoTests.m
+//  MPVDemoTests
+//
+//  Created by liuxiaobing on 2018/9/28.
+//  Copyright © 2018 dream. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MPVDemoTests : XCTestCase
+
+@end
+
+@implementation MPVDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
